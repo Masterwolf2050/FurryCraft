6731201fb8db4cf342384f7c0a28931e58685d86
Masterwolf2050 <masterwolf2050@gmail.com>
2014-03-01 17:29:58 -0600

    New version

---
+++ src/main/java/com/blackfang/fc/proxy/Clientproxy.java
@@ -0,0 +1,23 @@
+package com.blackfang.fc.proxy;
+
+import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.client.registry.KeyBindingRegistry;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.common.network.PacketDispatcher;
+import cpw.mods.fml.common.registry.TickRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.common.ForgeDirection;
+import net.minecraftforge.common.MinecraftForge;
+
+@SideOnly(Side.CLIENT)
+public class ClientProxy extends CommonProxy
+{
+	
+}
---
+++ src/main/java/com/blackfang/fc/proxy/Serverproxy.java
@@ -0,0 +1,6 @@
+package com.blackfang.fc.proxy;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ForgeDirection;
